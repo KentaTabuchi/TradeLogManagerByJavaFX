1ef4ec4db62af40d58373817c4f7585e1f2bf919
田淵健太 <tabuchikenta@tabuchikenta.local>
2017-09-05 11:59:40 +0900

    Making the event 'show addTradeLogStage'  has completed.

--- src/application/TradeLogTableStageController.java
+++ src/application/TradeLogTableStageController.java
@@ -3,8 +3,13 @@
  */
 package application;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -14,6 +19,19 @@ import javafx.stage.Stage;
 public class TradeLogTableStageController {
 	@FXML protected void onShowAddLogWindowMenuClick(ActionEvent evt){
 		System.out.println("starting onShowAddLogWindowMenuClick was successed.");
-	  
+	  Stage addLogStage = new Stage();
+	  BorderPane root;
+	try {
+		root = (BorderPane)FXMLLoader.load(getClass().getResource("AddLogStage.fxml"));
+		Scene scene = new Scene(root,400,400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		addLogStage.setScene(scene);
+		addLogStage.show();
+		addLogStage.setX(addLogStage.getX()+400);
+	} catch (IOException e) {
+		// TODO 自動生成された catch ブロック
+		e.printStackTrace();
+	}
+
 	}
 }
